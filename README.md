ce99e27c48d5e410c37af6222a69a7aa9180d5dd
JuJo <robin.feuerer@gmx.de>
2023-05-24 23:24:11 +0200

    Added more Default Support so it will likeley work without changing anything

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-you will need to set the RiotGames directory in the autovalorant.bat file
+you will need to set the RiotGames directory in the autovalorant.bat file in line 2 
--- autovalorant.bat
+++ autovalorant.bat
@@ -1,13 +1,23 @@
 @echo off
-set "valo=C:\Riot Games"
 
+for %%D in ( C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    if exist "%%D:\Riot Games\" (
+        set "valo=%%D:\Riot Games"
+        goto :found_folder
+    )
+)
+
+echo Riot Games folder not found.
+pause
+exit
+
+:found_folder
 copy /y "%~dp0\jp\old\*.*" "%valo%\VALORANT\live\ShooterGame\Content\Paks"
 copy /y "%~dp0\wallpaper\old\*.*" "%valo%\VALORANT\live\ShooterGame\Content\Movies\Menu"
 
 start "" "%valo%\Riot Client\RiotClientServices.exe" --launch-product=valorant --launch-patchline=live"
 
 :start
-
 tasklist /fi "imagename eq Valorant.exe" 2>NUL | find /i /n "Valorant.exe" >NUL
 if "%ERRORLEVEL%"=="0" goto :copy_files
 
